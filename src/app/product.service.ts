ad3808e3a7fc0847b9f6b8102ff28ff2bb04c8db
Toan Le <toanle@mac-mini.lan>
2020-09-10 23:09:29 -0400

    Create a getAlbum() method in ProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
+  private _albumURL : string = '../assets/album.json'
+
+  getAlbum(id: number)  {
+    this._http.get(this._albumURL)
+  }
 
 }
