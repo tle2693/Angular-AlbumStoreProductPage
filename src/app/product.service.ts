f777eccc747dc361c3c6cc6bcc0eaacfab3cb6b3
Toan Le <toanle@mac-mini.lan>
2020-09-10 23:29:43 -0400

    Import productService to productDescription

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { }
-  private _albumURL : string = '../assets/album.json'
+  private _albumURL : string = '../assets/album.json';
 
+  constructor(private _http: Http) { }
+  
   getAlbum(id: number)  {
-    this._http.get(this._albumURL)
+    return this._http.get(this._albumURL).map((response) => response.json());
   }
 
 }
